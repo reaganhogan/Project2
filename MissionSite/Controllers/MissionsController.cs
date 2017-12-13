0dd24352431eda127bb9019f0cdf9babd8388ba5
reaganhogan <reaganhogan15@gmail.com>
2017-12-12 19:56:05 -0700

    first commit

---
+++ MissionSite/Controllers/MissionsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MissionSite.Controllers
+{
+    public class MissionsController : Controller
+    {
+        // GET: Missions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Missions
+        //passes parameters to the MissionInfo view when it gets directed towards
+        public ActionResult MissionInfo(string Language, String MissionName, string PresidentName, string Address, string Climate, string Religion, string Picture)
+        {
+            //Defines mission info in viewbag
+            ViewBag.MissionName = MissionName;
+            ViewBag.PresidentName = PresidentName;
+            ViewBag.Address = Address;
+            ViewBag.Climate = Climate;
+            ViewBag.Religion = Religion;
+            ViewBag.Picture = Picture;
+            ViewBag.Language = Language;
+            return View();
+        }
+        //this is me practicing comments
+        //This form will need more work once we submit info from the form, right now it is a reset form
+        [HttpPost]
+        public ActionResult FAQAnswers(FormCollection form)
+        {
+            // String reply = form["New Reply"].ToString();
+            // form.Clear();
+            return View();
+        }
+    }
+}
---
+++ MissionSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MissionSite.Startup))]
+namespace MissionSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
